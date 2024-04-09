2ff21f551f6c55b801c55737fd43b56fb1020634
Matt Bremmers <bremmdev@gmail.com>
2024-04-09 18:58:27 +0200

    add loading spinner for page

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageSpinner from "../components/ui/PageSpinner";
+
+const loading = () => {
+  return <PageSpinner />;
+};
+
+export default loading;
---
+++ src/components/ui/PageSpinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageSpinner = () => {
+  return (
+    <div className='flex justify-center my-12 mx-auto'><span className="animate-spin w-10 h-10 rounded-full border-2 border-slate-300 border-t-slate-600"></span></div>
+  )
+}
+
+export default PageSpinner
